86204362061831c59e06be876b4095070551f0dc
James Villarrubia <james.villarrubia@gmail.com>
2023-12-27 16:17:14 -0500

    update

--- src/services/chats/chats.class.js
+++ src/services/chats/chats.class.js
@@ -1,12 +1,92 @@
-import getOpenai from "../utils/cacheProxy.js";
-
+import openai from "../utils/cacheProxy.js";
 
 
 // /*******************************
 //  * MERGING
 //  *******************************/
+// import pkg from 'lodash';
+// const { set, get } = pkg;
+// const deltaPaths = ['choices[0].delta'];
+// const concatenatedPaths = ['choices[*].content', 'choices[*].tool_calls[*].function.arguments'];
+// const streamPrintPath = 'choices[0].delta.content'
+// function setDeltaProp(delta, deltaParentPath, response, concatenatedPaths, index, key) {
+//     const deltaProp = delta[key];
+//     const newConcatPaths = concatenatedPaths.map((v) => v.replace('[*]', `[${index}]`));
+//     const currentProp = get(response, deltaParentPath);
+
+//     if (Array.isArray(deltaProp)) {
+//         deltaProp.forEach(({ index, ...rest }) => {
+//             Object.keys(rest).forEach(key =>
+//                 setDeltaProp(rest, `${deltaParentPath}[${index}].${key}`, response, newConcatPaths, index, key));
+//         });
+//     } else if (deltaProp && newConcatPaths.includes(deltaParentPath)) {
+//         set(response, deltaParentPath, (currentProp || '') + deltaProp);
+//     } else if (typeof deltaProp === 'object' && deltaProp !== null) {
+//         Object.keys(deltaProp).forEach(key =>
+//             setDeltaProp(deltaProp, `${deltaParentPath}.${key}`, response, newConcatPaths, index, key));
+//     } else {
+//         set(response, deltaParentPath, deltaProp);
+//     }
+// }
+
+// export function handleDeltas(object, response) {
+//     const delta = get(object, deltaPaths[0]);
+//     const deltaParentPath = deltaPaths[0].slice(0, -6);
+
+//     if (delta) {
+//         Object.keys(delta).forEach(key => setDeltaProp(delta, `${deltaParentPath}.${key}`, response, concatenatedPaths, 0, key));
+//     }
+//     return response
+// }
 
 
+
+
+
+
+
+
+
+
+export const makeRequest = async (options)=>{
+  return openai.chat.completions.create(options);
+}
+
+
+
+
+// export const processConversation = async (conversation, tools, tool, writeFunc) => {
+
+//   let options = {
+//     model: 'gpt-4-1106-preview',
+//     stream: true,
+//     messages:conversation, 
+//     tools, 
+//     tool_choice: tool
+//   }
+//   // if its use local, then you need to run call Tools
+//   options.tools = Object.values(availableTools)
+//   let response = await makeRequest(options)
+//   let mergeObject = {}
+//   if(options.stream){
+//     for await (const chunk of response) {
+//       mergeObject = handleDeltas(chunk, mergeObject)
+//       writeFunc(chunk)
+//     }
+//     conversation.push(mergeObject.choices[0])
+//   }else{
+//     mergeObject = response
+//     writeFunc(mergeObject)
+//   }
+
+
+//     let functionResults = await callTools(mergeObject.choices[0].tool_calls);
+//     if (functionResults.length) {
+//       conversation.push(...functionResults)
+//       conversation = await processConversation(conversation, tools, tool, writeFunc)
+//     }
+//   return conversation
+// };
 
 
 
@@ -15,8 +95,6 @@ export class ChatService {
     this.options = options
   }
 
-
-  
 
   async create(data, params) {
     console.log('\n\n************ CHAT SERVICE CREATE')
@@ -40,6 +118,5 @@ export class ChatService {
 }
 
 export const getOptions = (app) => {
-
   return { app }
 }
