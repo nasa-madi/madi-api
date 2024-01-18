047532ee39048849986ec2bc50f24bef8b208f8f
James Villarrubia <james.villarrubia@gmail.com>
2024-01-18 16:27:43 -0500

    debugging cloudrun

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,19 @@
 import { app } from './src/app.js'
 
 // Load our database connection info from the app configuration
-let config = app.get('postgresql')
-console.log(config)
-console.log(app.get('file'))
+// let config = app.get('postgresql')
+// console.log(config)
+// console.log(app.get('file'))
+
+let config = {
+    client: 'pg',
+    connection: {
+      host: '10.43.177.3',
+      port: '5432',
+      user: 'unicorn_user', 
+      password: 'magical_password',
+      database: 'rainbow_database', 
+    },
+}
+
 export default config
