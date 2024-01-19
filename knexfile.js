d90fc9a01d359453f9bab7358a7e21633db73209
James Villarrubia <james.villarrubia@gmail.com>
2024-01-19 00:02:19 -0500

    updates pooling settings for knex and postgres

--- knexfile.js
+++ knexfile.js
@@ -2,22 +2,5 @@
 import { app } from './src/app.js'
 
 // Load our database connection info from the app configuration
-// let config = app.get('postgresql')
-// console.log(config)
-// console.log(app.get('file'))
-
-let config = {
-    client: 'pg',
-    connection: {
-      host: '10.43.177.3',
-      port: '5432',
-      user: 'postgres', 
-      password: 'changeme',
-      database: 'main', 
-    },
-    debug:true,
-    pool: { min: 0, max: 30, acquireTimeoutMillis: 10 * 1000 },
-}
-console.log(config)
-
+let config = app.get('postgresql')
 export default config
