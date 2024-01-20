7dbb4b5a8aa8076e5e80208956a269900c4bc4f6
James Villarrubia <james.villarrubia@gmail.com>
2024-01-20 11:25:32 -0500

    updates tool list

--- src/services/chats/chats.class.js
+++ src/services/chats/chats.class.js
@@ -6,15 +6,18 @@ export class ChatService {
 
   async makeRequest(options){
     let openai = this.options?.app?.openai
+    console.log(options)
     return openai.chat.completions.create(options);
   }
   
 
   async create(data, params) {
     console.log('\n\n************ CHAT SERVICE CREATE')
-    console.log("model: ", data.model)
-    console.log("messages: ",data.messages)
-    console.log("tool_choice: ",data.tool_choice || "auto")
+    console.log(data)
+    console.log(params)
+    // console.log("model: ", data.model)
+    // console.log("messages: ",data.messages)
+    // console.log("tool_choice: ",data.tool_choice || "auto")
     // console.log("tools: ", availableToolDescriptions)
     let { messages, tools, tool_choice } = data
     let stream = !!data.stream
