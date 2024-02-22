9f8f2794cdfe25ac140717541c625dae604f1bd3
James Villarrubia <james.villarrubia@gmail.com>
2024-02-22 09:35:35 -0500

    Disable Cloud Storage

--- src/services/index.js
+++ src/services/index.js
@@ -1,22 +1,20 @@
-
-
-import { chunks } from './chunks/chunks.js'
-import { upload } from './uploads/uploads.js'
+// import { sharedAi } from './shared-ai/shared-ai.js'
+// import { chunks } from './chunks/chunks.js'
+// import { upload } from './uploads/uploads.js'
 import { tool } from './tools/tools.js'
 import { chat } from './chats/chats.js'
-import { document } from './documents/documents.js'
+// import { document } from './documents/documents.js'
 import { user } from './users/users.js'
-import { blob } from './blobs/blobs.js'
+// import { blob } from './blobs/blobs.js'
 
 export const services = (app) => {
-
-  
-  app.configure(blob)
-  app.configure(chunks)
-  app.configure(upload)
+  // app.configure(sharedAi)
+  // app.configure(blob)
+  // app.configure(chunks)
+  // app.configure(upload)
   app.configure(tool)
   app.configure(chat)
-  app.configure(document)
+  // app.configure(document)
   app.configure(user)
   // All services will be registered here
 }
