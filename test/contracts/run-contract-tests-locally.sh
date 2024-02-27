2f44a8cb1043ae9f43ce6de4afc4a472b8b25072
James Villarrubia <james.villarrubia@gmail.com>
2024-02-27 12:52:37 -0500

    attempts to fix dockertest

--- test/contracts/run-contract-tests-locally.sh
+++ test/contracts/run-contract-tests-locally.sh
@@ -6,7 +6,7 @@ compose_file="${script_dir}/../../.cicd/contract.docker-compose.yml"
 
 # Check if OPENAI_API_KEY is set
 if [[ -z "${OPENAI_API_KEY}" ]]; then
-  echo "OPENAI_API_KEY environment variable is not set. Run \`export=OPENAI_API_KEY=XXXXX\` before continuing."
+  echo "OPENAI_API_KEY environment variable is not set. Run \`export OPENAI_API_KEY=XXXXX\` before continuing."
   exit 1
 fi
 
