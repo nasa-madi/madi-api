de74f38f038602556f1bf7fd4d21665927813bf6
James Villarrubia <james.villarrubia@gmail.com>
2024-03-05 16:10:34 -0500

    Distinguishes internal from external Error messaging.

--- src/hooks/log-error.js
+++ src/hooks/log-error.js
@@ -10,6 +10,40 @@ export const logError = async (context, next) => {
       logger.error('Data: %O', error.data)
     }
 
+    if(error.cleanMessage){
+      error.message = error.cleanMessage
+      error.cleanMessage = undefined
+    }
     throw error
   }
+}
+
+
+export const logErrorExternal = async (context, next) => {
+    throw rewriteErrorMessage(context.error);
+}
+
+function rewriteErrorMessage(error) {
+    // Define the match and message pairs
+    let matchMessagePairs = [
+        {
+            match: 'duplicate key value violates unique constraint',
+            message: 'You cannot create a duplicate entity.'
+        },
+        // Add more pairs here as needed
+    ];
+
+    // Loop through each pair
+    for (let pair of matchMessagePairs) {
+        // Create a regular expression from the match string
+        let regex = new RegExp(pair.match);
+
+        // Test the error message against the regular expression
+        if (regex.test(error.message)) {
+            // If there's a match, replace the error message and exit the loop
+            error.cleanMessage = pair.message;
+            break;
+        }
+    }
+    return error
 }
