8f1db5bd1cc2a36fca6ac9148552bc944d366b05
James Villarrubia <james.villarrubia@gmail.com>
2024-08-19 23:28:56 -0400

    updates bc postman isn't saving.

--- test/contracts/run-contract-tests-locally.sh
+++ test/contracts/run-contract-tests-locally.sh
@@ -9,7 +9,7 @@ if [[ -z "${OPENAI_API_KEY}" ]]; then
   exit 1
 fi
 
-CONTRACT_TYPE=blueprint     OPENAI_API_KEY=$OPENAI_API_KEY docker compose -f ./test/contracts/contract.docker-compose.yml up --exit-code-from newman
+CONTRACT_TYPE=blueprint     OPENAI_API_KEY=$OPENAI_API_KEY docker compose -f ./test/contracts/contract.docker-compose.yml up --exit-code-from newman --build --force-recreate
 # CONTRACT_TYPE=blueprint docker compose -f ./test/contracts/contract.docker-compose.yml down --volumes
 
 # CONTRACT_TYPE=plugin-loader OPENAI_API_KEY=$OPENAI_API_KEY docker compose -f ./test/contracts/contract.docker-compose.yml up --exit-code-from newman
